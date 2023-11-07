aa70986c8270a4832d9b488c4e98036dd79a2597
Nicholas Galante <nicholasgalante94@gmail.com>
2023-11-07 15:52:18 -0500

    feat(db): add columns to the students table - Add mentor_id column of type integer - Add school_name column of type string - Add degree_type column of type string - Add area_of_study column of type string - Add expected_graduation column of type date - Add status column of type string

--- db/migrate/20231107035542_create_students.rb
+++ db/migrate/20231107035542_create_students.rb
@@ -1,7 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
     create_table :students do |t|
-
+      t.integer :mentor_id
+      t.string :school_name
+      t.string :degree_type
+      t.string :area_of_study
+      t.date :expected_graduation
+      t.string :status
       t.timestamps
     end
   end
