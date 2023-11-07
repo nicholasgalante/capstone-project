37ced2593134f34aba8fa1834aa6ad82e3b3d1ce
Nicholas Galante <nicholasgalante94@gmail.com>
2023-11-07 16:27:30 -0500

    fix(db/migrate/20231107035542_create_students.rb): set default value for mentor_id column to nil fix(db/migrate/20231107035542_create_students.rb): set default value for status column to "incomplete"

--- db/migrate/20231107035542_create_students.rb
+++ db/migrate/20231107035542_create_students.rb
@@ -1,12 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
     create_table :students do |t|
-      t.integer :mentor_id
+      t.integer :mentor_id, default: nil
       t.string :school_name
       t.string :degree_type
       t.string :area_of_study
       t.date :expected_graduation
-      t.string :status
+      t.string :status, default: "incomplete"
       t.timestamps
     end
   end
