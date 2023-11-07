900fadebcf76d8aa55e3019a0912b6da04fd8079
Nicholas Galante <nicholasgalante94@gmail.com>
2023-11-07 16:00:37 -0500

    chore(db/migrate): delete create_comments migration file feat(db/migrate): add columns meeting_id, user_id, and confirmed to create_user_meetings migration file

--- db/migrate/20231107035606_create_user_meetings.rb
+++ db/migrate/20231107035606_create_user_meetings.rb
@@ -1,7 +1,9 @@
 class CreateUserMeetings < ActiveRecord::Migration[6.1]
   def change
     create_table :user_meetings do |t|
-
+      t.integer :meeting_id
+      t.integer :user_id
+      t.boolean :confirmed
       t.timestamps
     end
   end
