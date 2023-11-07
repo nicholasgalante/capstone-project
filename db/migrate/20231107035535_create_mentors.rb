c635264d1d27399457fc4961225d9dca570aa523
Nicholas Galante <nicholasgalante94@gmail.com>
2023-11-07 15:46:00 -0500

    gitIgnore API Key

--- db/migrate/20231107035535_create_mentors.rb
+++ db/migrate/20231107035535_create_mentors.rb
@@ -1,7 +1,8 @@
 class CreateMentors < ActiveRecord::Migration[6.1]
   def change
     create_table :mentors do |t|
-
+      t.string :company_name
+      t.string :status
       t.timestamps
     end
   end
