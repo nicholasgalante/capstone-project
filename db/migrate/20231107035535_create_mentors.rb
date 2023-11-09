484dd1d9614467452f3b7c90cdeb1a680e4d6bfe
Nicholas Galante <nicholasgalante94@gmail.com>
2023-11-08 19:58:00 -0500

    feat(migration): add job_title column to mentors table fix(migration): set default value for status column in mentors table to "incomplete"

--- db/migrate/20231107035535_create_mentors.rb
+++ db/migrate/20231107035535_create_mentors.rb
@@ -2,7 +2,8 @@ class CreateMentors < ActiveRecord::Migration[6.1]
   def change
     create_table :mentors do |t|
       t.string :company_name
-      t.string :status
+      t.string :job_title
+      t.string :status, default: "incomplete"
       t.timestamps
     end
   end
