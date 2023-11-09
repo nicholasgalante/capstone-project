e050a3e5137e542674837c083e6495612b433ac7
Nicholas Galante <nicholasgalante94@gmail.com>
2023-11-08 20:29:07 -0500

    feat(seeds.rb): Add mentor and student seed data feat(seeds.rb): Add mentor1 and mentor2 feat(seeds.rb): Add student1 and student2 feat(seeds.rb): Add meeting1 and meeting2 feat(seeds.rb): Add UserMeeting data for meeting1 and meeting2

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,7 @@ end
 
 puts "🌱 Seeding users..."
 
-
-u1 = Mentor.create(
+user1 = Mentor.create(
   first_name: "Isabella",
   last_name: "Clark",
   email_address: "mentor1@example.com",
@@ -23,10 +22,10 @@ u1 = Mentor.create(
   password_digest: BCrypt::Password.create("password"),
   company_name: "TechIndustry",
   job_title: "Senior Software Engineer",
-  status: "complete"
+  status: "complete",
 )
 
-u2 = Mentor.create(
+user2 = Mentor.create(
   first_name: "Elijah",
   last_name: "Ramirez",
   email_address: "mentor2@example.com",
@@ -34,33 +33,73 @@ u2 = Mentor.create(
   password_digest: BCrypt::Password.create("password"),
   company_name: "",
   job_title: "",
-  status: "incomplete"
+  status: "incomplete",
 )
 
-u3 = Student.create(
-   first_name: "Oliva",
-   last_name: "Parker",
-   email_address: "student1@example.com",
-   type: "student",
-   password_digest: BCrypt::Password.create("password"),
-   mentor_id: u1.id,
-   school_name: "CUNY Hunter College",
-   degree_type: "Bachelors of Science",
-   area_of_study: "Computer Science",
-   expected_graduation: "07/01/2025",
-   status: "complete",
+user3 = Student.create(
+  first_name: "Oliva",
+  last_name: "Parker",
+  email_address: "student1@example.com",
+  type: "student",
+  password_digest: BCrypt::Password.create("password"),
+  mentor_id: user1.id,
+  school_name: "CUNY Hunter College",
+  degree_type: "Bachelors of Science",
+  area_of_study: "Computer Science",
+  expected_graduation: "07/01/2025",
+  status: "complete",
+)
+
+user4 = Student.create(
+  first_name: "Noah",
+  last_name: "Foster",
+  email_address: "student2@example.com",
+  type: "student",
+  password_digest: BCrypt::Password.create("password"),
+  mentor_id: user1.id,
+  school_name: "CUNY City college",
+  degree_type: "Bachelors of Science",
+  area_of_study: "Computer Engineering",
+  expected_graduation: "",
+  status: "incomplete",
+)
+
+meeting1 = Meeting.create(
+  organizer_id: user3.id,
+  meeting_datetime: "2023-11-01T16:00:00-05:00",
+  location: "Hunter Campus Library",
+  topics_discussed: "Single table inheritance and entity relationship diagrams.",
+  next_steps: "Start studing for your AWS certification on Udemy!",
+)
+
+meeting2 = Meeting.create(
+  organizer_id: user3.id,
+  meeting_datetime: "2023-11-17T13:00:00-05:00",
+  location: "Hunter Campus Library",
+  topics_discussed: "",
+  next_steps: "",
+)
+
+UserMeeting.create(
+  meeting_id: meeting1.id,
+  user_id: user3.id,
+  confirmed: true,
+)
+
+UserMeeting.create(
+  meeting_id: meeting1.id,
+  user_id: user1.id,
+  confirmed: true,
+)
+
+UserMeeting.create(
+   meeting_id: meeting2.id,
+   user_id: user3.id,
+   confirmed: true,
  )
  
- u4 = Student.create(
-   first_name: "Noah",
-   last_name: "Foster",
-   email_address: "student2@example.com",
-   type: "student",
-   password_digest: BCrypt::Password.create("password"),
-   mentor_id: u1.id,
-   school_name: "CUNY City college",
-   degree_type: "Bachelors of Science",
-   area_of_study: "Computer Engineering",
-   expected_graduation: "",
-   status: "incomplete",
+ UserMeeting.create(
+   meeting_id: meeting2.id,
+   user_id: user1.id,
+   confirmed: false,
  )
