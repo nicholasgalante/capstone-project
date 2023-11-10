af2f08b1792b660edeeec9c287e7a242176f6e63
Nicholas Galante <nicholasgalante94@gmail.com>
2023-11-09 23:29:35 -0500

    chore(user.rb): add validations for first_name, last_name, email_address, type, and password_digest in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
    has_secure_password
-   
+
    has_many :user_meetings
    has_many :meetings, through: :user_meetings
+
+   validates :first_name, presence: true, length: { maximum: 25 }
+   validates :last_name, presence: true, length: { maximum: 25 }
+   validates :email_address, presence: true, length: { maximum: 255 },
+                             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                             uniqueness: { case_sensitive: false }
+   validates :type, inclusion: { in: ['student', 'mentor'] }
+   validates :password_digest, presence: true, length: { minimum: 6 }
 end
