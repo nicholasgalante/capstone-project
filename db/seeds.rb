0484f82bea28e0dd7ee5bff5f2a6b675fe370255
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-07 11:53:33 -0500

    fix(seeds.rb): change type field values from lowercase to uppercase feat(seeds.rb): rename status field to application_status

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,50 +18,50 @@ user1 = Mentor.create(
   first_name: "Isabella",
   last_name: "Clark",
   email_address: "mentor1@example.com",
-  type: "mentor",
+  type: "Mentor",
   password_digest: BCrypt::Password.create("password"),
   company_name: "TechIndustry",
   job_title: "Senior Software Engineer",
-  status: "complete",
+  application_status: "complete",
 )
 
 user2 = Mentor.create(
   first_name: "Elijah",
   last_name: "Ramirez",
   email_address: "mentor2@example.com",
-  type: "mentor",
+  type: "Mentor",
   password_digest: BCrypt::Password.create("password"),
   company_name: "",
   job_title: "",
-  status: "incomplete",
+  application_status: "incomplete",
 )
 
 user3 = Student.create(
   first_name: "Oliva",
   last_name: "Parker",
   email_address: "student1@example.com",
-  type: "student",
+  type: "Student",
   password_digest: BCrypt::Password.create("password"),
   mentor_id: user1.id,
   school_name: "CUNY Hunter College",
   degree_type: "Bachelors of Science",
   area_of_study: "Computer Science",
   expected_graduation: "07/01/2025",
-  status: "complete",
+  application_status: "complete",
 )
 
 user4 = Student.create(
   first_name: "Noah",
   last_name: "Foster",
   email_address: "student2@example.com",
-  type: "student",
+  type: "Student",
   password_digest: BCrypt::Password.create("password"),
   mentor_id: user1.id,
   school_name: "CUNY City college",
   degree_type: "Bachelors of Science",
   area_of_study: "Computer Engineering",
   expected_graduation: "",
-  status: "incomplete",
+  application_status: "incomplete",
 )
 
 meeting1 = Meeting.create(
