259c6b35e7b5cd77c0af4d3e3f2736869f331c57
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-07 21:39:31 -0500

    feat(create_users.rb): add 'matched' boolean column with default value of false

--- db/migrate/20231107035513_create_users.rb
+++ db/migrate/20231107035513_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :email_address
       t.string :password_digest
       t.string :role
+      t.boolean :matched, default: :False
       t.timestamps
     end
   end
