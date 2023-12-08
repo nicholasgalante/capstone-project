96118ac6031c253df3e5bf7ff9790daed2c1d9e1
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-07 21:40:43 -0500

    chore(db/migrate): remove 'confirmed' column from 'user_meetings' table

--- db/migrate/20231107035606_create_user_meetings.rb
+++ db/migrate/20231107035606_create_user_meetings.rb
@@ -3,7 +3,6 @@ class CreateUserMeetings < ActiveRecord::Migration[6.1]
     create_table :user_meetings do |t|
       t.belongs_to :user
       t.belongs_to :meeting
-      t.boolean :confirmed
       t.timestamps
     end
   end
