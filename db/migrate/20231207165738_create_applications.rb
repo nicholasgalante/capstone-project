97f04bc7a5d6c6b0300d3d3815c3c4676728d134
Nicholas Galante <nicholasgalante94@gmail.com>
2023-12-07 20:51:53 -0500

    chore(migration): remove application_status column from create_users migration feat(migration): add application_status and application columns to create_applications migration

--- db/migrate/20231207165738_create_applications.rb
+++ db/migrate/20231207165738_create_applications.rb
@@ -1,7 +1,8 @@
 class CreateApplications < ActiveRecord::Migration[6.1]
   def change
     create_table :applications do |t|
-      
+      t.string :application_status, default: "incomplete"
+      t.text :application 
       t.timestamps
     end
   end
